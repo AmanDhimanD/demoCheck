7bf0cf4528023872babef02b5c4a4cb617f5e03a
AmanDhimanD <dhiman.1999.aman@gmail.com>
2023-07-11 11:23:49 +0530

    InvestigationalTextRemove

--- platform/ui/src/views/StudyList/components/Header.js
+++ platform/ui/src/views/StudyList/components/Header.js
@@ -18,9 +18,7 @@ function Header({ appLogo = OHIFLogo(), children, t }) {
         <div>{children}</div>
       </div>
       <div className="flex items-center">
-        <span className="mr-3 text-common-light text-lg">
-          {t('FOR INVESTIGATIONAL USE ONLY')}
-        </span>
+        <span className="mr-3 text-common-light text-lg">{t('')}</span>
         <IconButton
           variant="text"
           color="inherit"
